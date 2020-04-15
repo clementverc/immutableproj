6681063470b1c64dae2b3aebde18cba8ec776de5
clement <clement.vercucque@orange.fr>
2020-04-15 13:51:39 +0200

    ajout de la fonctionnalitée de filtrage/recherche de contact + tri par ordre alphabétique des noms

--- src/components/contacts/index.js
+++ src/components/contacts/index.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+import Grid from '@material-ui/core/Grid';
+
 import IconButton from '@material-ui/core/IconButton';
 import DeleteIcon from '@material-ui/icons/Delete';
+import CreateIcon from '@material-ui/icons/Create';
 import ListItem from '@material-ui/core/ListItem';
 import PersonAddIcon from '@material-ui/icons/PersonAdd';
 import ListItemText from '@material-ui/core/ListItemText';
 import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import Avatar from '@material-ui/core/Avatar';
 import FaceIcon from '@material-ui/icons/Face';
-import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
-import SettingsIcon from '@material-ui/icons/Settings';
 import SendIcon from '@material-ui/icons/Send';
 import TextField from '@material-ui/core/TextField';
 import Tooltip from '@material-ui/core/Tooltip';
@@ -28,17 +29,26 @@ const Contact = ({ user }) => {
 
   return (
     <div>
-      <ListItem>
-        <ListItemAvatar>
-          <Avatar>
-            <FaceIcon />
-          </Avatar>
-        </ListItemAvatar>
-        <ListItemText primary={`${firstName} ${lastName} ${id}`} secondary={`${phone} - ${city}`} />
-        <IconButton color="secondary" onClick={() => deleteContact(id)}>
-          <DeleteIcon />
-        </IconButton>
-      </ListItem>
+      <Grid container spacing={3}>
+        <Grid item sm={6} md={3}>
+          {/* <Container> */}
+          <ListItem>
+            <ListItemAvatar>
+              <Avatar>
+                <FaceIcon />
+              </Avatar>
+            </ListItemAvatar>
+            <ListItemText primary={`${firstName} ${lastName} ${id}`} secondary={`${phone} - ${city}`} />
+            <IconButton color="secondary" onClick={() => deleteContact(id)}>
+              <CreateIcon />
+            </IconButton>
+            <IconButton color="secondary" onClick={() => deleteContact(id)}>
+              <DeleteIcon />
+            </IconButton>
+          </ListItem>
+          {/* </Container> */}
+        </Grid>
+      </Grid>
     </div>
   );
 };
@@ -54,8 +64,10 @@ class Contacts extends Component {
         phone: '',
         id: '',
       },
+      dataSearch: '',
     };
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.search = this.search.bind(this);
   }
 
   handleSubmit(event) {
@@ -73,34 +85,25 @@ class Contacts extends Component {
     // console.log(event.target.firstName.value);
   }
 
+  search(event) {
+    const { value } = event.target;
+    this.setState({ dataSearch: value.toLowerCase() });
+  }
+
   render() {
     const { items } = this.props;
-    const { form } = this.state;
+    const { form, dataSearch } = this.state;
+    items.sort((a, b) => (a.lastName.toLowerCase() < b.lastName.toLowerCase() ? -1 : 1));
 
     return (
       <div>
         <h1>
-          Contacts
-          <IconButton>
-            <ArrowDropDownIcon />
-          </IconButton>
-          <Tooltip title="Ajouter contact toto">
-            <IconButton onClick={() => addContact(
-              {
-                id: 99,
-                firstName: 'toto',
-                lastName: 'touto',
-                phone: '0123456789',
-                city: 'chezLui',
-              },
-            )}
-            >
+          Mes Contacts
+          <Tooltip title="Ajouter un contact">
+            <IconButton>
               <PersonAddIcon />
             </IconButton>
           </Tooltip>
-          <IconButton>
-            <SettingsIcon />
-          </IconButton>
         </h1>
         <h3>Ajouter un contact :</h3>
         <form onSubmit={this.handleSubmit}>
@@ -115,10 +118,15 @@ class Contacts extends Component {
             </IconButton>
           </Tooltip>
         </form>
+        <input onChange={this.search} className="search" placeholder="Rechercher un contact" />
+        <h1>{dataSearch}</h1>
         <ul>
-          {items.map((user) => (
-            <Contact key={user.id} user={user} />
-          ))}
+          { dataSearch === '' ? items.map((user) => (<Contact key={user.id} user={user} />))
+            : items.filter((user) => user.firstName.toLowerCase().indexOf(dataSearch) !== -1
+            || user.lastName.toLowerCase().indexOf(dataSearch) !== -1
+            || user.phone.toLowerCase().indexOf(dataSearch) !== -1
+            || user.city.toLowerCase().indexOf(dataSearch) !== -1)
+              .map((user) => (<Contact key={user.id} user={user} />))}
         </ul>
       </div>
     );
