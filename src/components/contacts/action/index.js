87407bdc30f51261f5f2eee21cd6b3e1544ab460
clement <clement.vercucque@orange.fr>
2020-04-17 18:48:17 +0200

    ajout de la modification d'un utilisateur

--- src/components/contacts/action/index.js
+++ src/components/contacts/action/index.js
@@ -1,33 +1,44 @@
 import actionsTypes from './actions-types';
 import store from '../../../store';
 
-/**
-* Add contact
-*/
 const actionContact = (contacts) => ({
   type: actionsTypes.ACTION_CONTACT,
   contacts,
 });
 
-// /**
-// * Delete contact
-// */
-// export const deleteContact = (id) => ({
-//   type: actionsTypes.DELETE_CONTACT,
-//   id,
-// });
-
+/**
+* Add contact
+*/
 export const addContact = (contact) => {
   const { items } = store.getState().contacts;
   items.push(contact);
   store.dispatch(actionContact(items));
 };
 
+/**
+* Delete contact
+*/
 export const deleteContact = (id) => {
   const { items } = store.getState().contacts;
   items.map((contact, i) => {
     if (id === contact.id) {
       items.splice(i, 1);
+    }
+    return null;
+  });
+  store.dispatch(actionContact(items));
+};
+
+/**
+* Update contact
+*/
+export const updateContact = (id, contactData) => {
+  const { items } = store.getState().contacts;
+  // console.log(id);
+  // console.log(contactData);
+  items.map((contact, i) => {
+    if (id === contact.id) {
+      items[i] = contactData;
     }
     return null;
   });
--- src/components/contacts/index.js
+++ src/components/contacts/index.js
@@ -5,9 +5,7 @@ import Grid from '@material-ui/core/Grid';
 
 import IconButton from '@material-ui/core/IconButton';
 import DeleteIcon from '@material-ui/icons/Delete';
-import CreateIcon from '@material-ui/icons/Create';
 import ListItem from '@material-ui/core/ListItem';
-import PersonAddIcon from '@material-ui/icons/PersonAdd';
 import ListItemText from '@material-ui/core/ListItemText';
 import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import Avatar from '@material-ui/core/Avatar';
@@ -15,8 +13,10 @@ import FaceIcon from '@material-ui/icons/Face';
 import SendIcon from '@material-ui/icons/Send';
 import TextField from '@material-ui/core/TextField';
 import Tooltip from '@material-ui/core/Tooltip';
-
-import { addContact, deleteContact } from './action';
+import Select from '@material-ui/core/Select';
+import InputLabel from '@material-ui/core/InputLabel';
+import FormControl from '@material-ui/core/FormControl';
+import { addContact, deleteContact, updateContact } from './action';
 
 const Contact = ({ user }) => {
   const {
@@ -30,23 +30,18 @@ const Contact = ({ user }) => {
   return (
     <div>
       <Grid container spacing={3}>
-        <Grid item sm={6} md={3}>
-          {/* <Container> */}
+        <Grid item sm={6} md={6}>
           <ListItem>
             <ListItemAvatar>
               <Avatar>
                 <FaceIcon />
               </Avatar>
             </ListItemAvatar>
-            <ListItemText primary={`${firstName} ${lastName} ${id}`} secondary={`${phone} - ${city}`} />
-            <IconButton color="secondary" onClick={() => deleteContact(id)}>
-              <CreateIcon />
-            </IconButton>
+            <ListItemText primary={`${firstName} ${lastName}`} secondary={`${phone} - ${city}`} />
             <IconButton color="secondary" onClick={() => deleteContact(id)}>
               <DeleteIcon />
             </IconButton>
           </ListItem>
-          {/* </Container> */}
         </Grid>
       </Grid>
     </div>
@@ -65,13 +60,15 @@ class Contacts extends Component {
         id: '',
       },
       dataSearch: '',
+      idSelectedUser: 0,
     };
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleUpdate = this.handleUpdate.bind(this);
+    this.handleChange = this.handleChange.bind(this);
     this.search = this.search.bind(this);
   }
 
   handleSubmit(event) {
-    // const { dispatch } = this.props;
     console.log(this);
     event.preventDefault();
     addContact({
@@ -82,7 +79,25 @@ class Contacts extends Component {
       phone: event.target.phone.value,
       id: event.target.id.value,
     });
-    // console.log(event.target.firstName.value);
+  }
+
+  handleChange(event) {
+    this.setState({ idSelectedUser: event.target.value });
+  }
+
+  handleUpdate(event) {
+    console.log(this);
+    event.preventDefault();
+    const idContact = Number(event.target.idSelectedUser.value);
+    // console.log(idContact);
+    updateContact(idContact, {
+      form: { },
+      firstName: event.target.firstNameEdit.value,
+      lastName: event.target.lastNameEdit.value,
+      city: event.target.cityEdit.value,
+      phone: event.target.phoneEdit.value,
+      id: idContact,
+    });
   }
 
   search(event) {
@@ -92,18 +107,12 @@ class Contacts extends Component {
 
   render() {
     const { items } = this.props;
-    const { form, dataSearch } = this.state;
-    items.sort((a, b) => (a.lastName.toLowerCase() < b.lastName.toLowerCase() ? -1 : 1));
-
+    const { form, dataSearch, idSelectedUser } = this.state;
+    items.sort((a, b) => (a.firstName.toLowerCase() < b.firstName.toLowerCase() ? -1 : 1));
     return (
       <div>
         <h1>
           Mes Contacts
-          <Tooltip title="Ajouter un contact">
-            <IconButton>
-              <PersonAddIcon />
-            </IconButton>
-          </Tooltip>
         </h1>
         <h3>Ajouter un contact :</h3>
         <form onSubmit={this.handleSubmit}>
@@ -118,8 +127,44 @@ class Contacts extends Component {
             </IconButton>
           </Tooltip>
         </form>
-        <input onChange={this.search} className="search" placeholder="Rechercher un contact" />
-        <h1>{dataSearch}</h1>
+        <h3>Modifier un contact :</h3>
+        <form onSubmit={this.handleUpdate}>
+          <FormControl>
+            <div>
+              <InputLabel htmlFor="idSelectedUser">Sélectionner un utilisateur</InputLabel>
+              <Select
+                native
+                value={idSelectedUser}
+                onChange={this.handleChange}
+                inputProps={{
+                  name: 'idSelectedUser',
+                  id: 'idSelectedUser',
+                }}
+              >
+                <option aria-label="None" value="" />
+                <option value="1">Jacques Last</option>
+                <option value="2">Pierre Last</option>
+                <option value="3">Ana Amop</option>
+                <option value="4">Cyril Vimard</option>
+                <option value="5">Jonathan Reza</option>
+                <option value="6">Pierre Feuille</option>
+                <option value="7">Octave Jean</option>
+                <option value="8">Cyril Second</option>
+              </Select>
+            </div>
+          </FormControl>
+          <TextField id="firstNameEdit" label="firstName" variant="outlined" />
+          <TextField id="lastNameEdit" label="fastName" variant="outlined" />
+          <TextField id="cityEdit" label="city" variant="outlined" />
+          <TextField id="phoneEdit" label="phone" type="number" InputLabelProps={{ shrink: true }} variant="outlined" />
+          <Tooltip title="Editer le contact">
+            <IconButton type="submit" value="Enregistrer">
+              <SendIcon />
+            </IconButton>
+          </Tooltip>
+        </form>
+        <input onChange={this.search} className="search" placeholder="Rechercher parmis les contacts" />
+        <h4>{dataSearch}</h4>
         <ul>
           { dataSearch === '' ? items.map((user) => (<Contact key={user.id} user={user} />))
             : items.filter((user) => user.firstName.toLowerCase().indexOf(dataSearch) !== -1
